e12121fade1c4ddb69410485a342148c1454ee1f
unknown <jklfaeifncaslkfj>
2018-10-13 02:23:37 -0700

    a

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://localhost:27017/time',  {useNewUrlParser: true})
+// mongoose.connect('mongodb://localhost:27017/time',  {useNewUrlParser: true})
+mongoose.connect('mongodb://todosuser:hjk786dfg@ds131151.mlab.com:31151/todos',  {useNewUrlParser: true})
 
 module.exports = {
     mongoose
